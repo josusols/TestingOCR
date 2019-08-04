11067682a0d29c7be19a1764132c8dfd30a347f2
Francisco Solís <josusols@gmail.com>
2019-08-04 16:41:14 -0600

    Add project files.

---
+++ testingOCR/f_main.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace testingOCR
+{
+    partial class F_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OPF_image = new System.Windows.Forms.OpenFileDialog();
+            this.BTN_analyze = new System.Windows.Forms.Button();
+            this.LL_uploadImage = new System.Windows.Forms.LinkLabel();
+            this.PB_image = new System.Windows.Forms.PictureBox();
+            this.RTB_response = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_analyze
+            // 
+            this.BTN_analyze.Location = new System.Drawing.Point(111, 39);
+            this.BTN_analyze.Name = "BTN_analyze";
+            this.BTN_analyze.Size = new System.Drawing.Size(116, 47);
+            this.BTN_analyze.TabIndex = 0;
+            this.BTN_analyze.Text = "Analyze";
+            this.BTN_analyze.UseVisualStyleBackColor = true;
+            this.BTN_analyze.Click += new System.EventHandler(this.BTN_analyze_Click);
+            // 
+            // LL_uploadImage
+            // 
+            this.LL_uploadImage.AutoSize = true;
+            this.LL_uploadImage.Location = new System.Drawing.Point(32, 56);
+            this.LL_uploadImage.Name = "LL_uploadImage";
+            this.LL_uploadImage.Size = new System.Drawing.Size(73, 13);
+            this.LL_uploadImage.TabIndex = 1;
+            this.LL_uploadImage.TabStop = true;
+            this.LL_uploadImage.Text = "Upload Image";
+            this.LL_uploadImage.Click += new System.EventHandler(this.LL_uploadImage_Click);
+            // 
+            // PB_image
+            // 
+            this.PB_image.Location = new System.Drawing.Point(12, 103);
+            this.PB_image.Name = "PB_image";
+            this.PB_image.Size = new System.Drawing.Size(232, 237);
+            this.PB_image.TabIndex = 2;
+            this.PB_image.TabStop = false;
+            // 
+            // RTB_response
+            // 
+            this.RTB_response.Location = new System.Drawing.Point(253, 12);
+            this.RTB_response.Name = "RTB_response";
+            this.RTB_response.Size = new System.Drawing.Size(228, 329);
+            this.RTB_response.TabIndex = 3;
+            this.RTB_response.Text = "";
+            // 
+            // F_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 353);
+            this.Controls.Add(this.RTB_response);
+            this.Controls.Add(this.PB_image);
+            this.Controls.Add(this.LL_uploadImage);
+            this.Controls.Add(this.BTN_analyze);
+            this.Name = "F_main";
+            this.Text = "Testing OCR";
+            ((System.ComponentModel.ISupportInitialize)(this.PB_image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OPF_image;
+        private System.Windows.Forms.Button BTN_analyze;
+        private System.Windows.Forms.LinkLabel LL_uploadImage;
+        private System.Windows.Forms.PictureBox PB_image;
+        private System.Windows.Forms.RichTextBox RTB_response;
+    }
+}
+
---
+++ testingOCR/f_main.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace testingOCR
+{
+    public partial class F_main : Form
+    {
+        PictureBox picB;
+        HttpClient client;
+        string keyAttr;
+        string endpointAttr;
+        string filePath;
+        
+        public F_main()
+        {
+            InitializeComponent();
+            keyAttr = ConfigurationManager.AppSettings.Get("computer-vision-key");
+            endpointAttr = ConfigurationManager.AppSettings.Get("endpoint");
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+        private void LL_uploadImage_Click(object sender, EventArgs e)
+        {
+            OPF_image.Title = "Open image";
+            OPF_image.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (OPF_image.ShowDialog() == DialogResult.OK)
+            {
+                picB = new PictureBox();
+                picB.Image = new Bitmap(OPF_image.FileName);
+                filePath = OPF_image.FileName;
+                PB_image.Image = picB.Image;
+            }
+        }
+
+        async private void BTN_analyze_Click(object sender, EventArgs e)
+        {
+            if (picB != null)
+            {
+                //Azure Computer Vision call.
+                client = new HttpClient();
+                var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+                // Request headers
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", keyAttr);
+                
+                // Request parameters
+                queryString["language"] = "en";
+                queryString["detectOrientation"] = "true";
+                var uri = endpointAttr + "/vision/v2.0/ocr?" + queryString;
+                
+                // Request body
+                HttpResponseMessage response;
+                
+                // Read the contents of the specified local image
+                // into a byte array.
+                byte[] byteData = GetImageAsByteArray(filePath);
+
+                // Add the byte array as an octet stream to the request body.
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    // This example uses the "application/octet-stream" content type.
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Asynchronously call the REST API method.
+                    response = await client.PostAsync(uri, content);
+                }
+
+                // Asynchronously get the JSON response.
+                var textResponse = await response.Content.ReadAsStringAsync();
+                var imgText = JsonConvert.DeserializeObject<imageText>(textResponse);
+                StringBuilder finalText = new StringBuilder();
+                foreach (var region in imgText.regions)
+                {
+                    foreach (var line in region.lines)
+                    {
+                        foreach (var word in line.words)
+                        {
+                            finalText.Append(word.text);
+                            finalText.Append(" ");
+                        }
+                        finalText.AppendLine();
+                    }
+                    finalText.AppendLine();
+                }
+                RTB_response.Text = textResponse;
+                MessageBox.Show(finalText.ToString(), ".::FINAL TEXT::.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Error, imagen expected", ".::ERROR::.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
